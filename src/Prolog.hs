2af031279806b32838c5075d2bc9a7e932747986
Mattox Beckman <mattox@illinois.edu>
2022-04-30 15:16:07 -0500

    First attempt to add a REPL.

--- src/Prolog.hs
+++ src/Prolog.hs
@@ -12,6 +12,7 @@ module Prolog
    , (+++)
    , consult, consultString, parseQuery
    , program, whitespace, comment, clause, terms, term, bottom, vname
+   , main, repl
    )
 where
 
@@ -19,3 +20,23 @@ import Syntax
 import Parser
 import Unifier
 import Interpreter
+
+main = do
+   putStrLn "Prolog Interpreter."
+   putStr "What file has the database? "
+   fname <- getLine
+   db <- consult fname
+   case db of
+     Left foo -> error $ show foo
+     Right bar -> repl bar
+
+repl :: Program -> IO ()
+repl program = do
+   putStr "> "
+   query <- getLine
+   case parseQuery query of
+     Left err -> putStrLn $ "Error: " ++ show err
+     Right q -> do result <- resolve program q
+                   putStrLn $ show result
+   repl program
+
